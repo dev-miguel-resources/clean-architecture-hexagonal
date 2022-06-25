8355b7de1321e692f8564bd1c49160913b14b318
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-06-24 20:23:31 -0400

    config and working with ts-mockito, jest/react-testing for tests

--- src/features/achiviements/application/get-all-achiviements-use-case.spec.ts
+++ src/features/achiviements/application/get-all-achiviements-use-case.spec.ts
@@ -1,2 +1,23 @@
-import { Achiviement } from "../domain/achiviement";
-import { AchiviementRepository } from "../domain/achiviement-repository";
+import { GetAllAchiviementsUseCase } from "./get-all-achiviements-use-case"
+import { mock, instance, when } from 'ts-mockito'
+import { AchiviementRepository } from "../domain/achiviement-repository"
+import { Achiviement } from "../domain/achiviement"
+
+describe('GetAllAchiviementsUseCase', () => {
+    it('should get all the achiviements', async () => {
+
+        //Given: Preparación del Mock
+        const achiviementRepository = mock<AchiviementRepository>()
+        const getAllAchiviementsUseCase = new GetAllAchiviementsUseCase(instance(achiviementRepository))
+        when(achiviementRepository.findAll()).thenResolve([
+            { id: 'sfn-sfin-aseb', name: 'Learn Hexagonal Architecture with TS', date: new Date('2022-06-22')},
+        ])
+
+        //When: Ejecución del test asociado al caso de uso
+        const actual = await getAllAchiviementsUseCase.execute()
+
+        //Then: Que verifica el sentido de assertion
+        expect(actual).toEqual<Achiviement[]>([
+            { id: 'sfn-sfin-aseb', name: 'Learn Hexagonal Architecture with TS', date: new Date('2022-06-22')}])
+    })
+})
