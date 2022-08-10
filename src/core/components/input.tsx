f98bda31ac116b3b8f7b7fd90aa79c81e0315675
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-08-09 22:03:20 -0400

    example component input

--- src/core/components/input.tsx
+++ src/core/components/input.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react"
-import styles from "./input.module.css"
-import { bind } from "../utils/bind"
+import { FC } from 'react'
+import styles from './input.module.css'
+import { bind } from '../utils/bind'
 
 const cx = bind(styles)
 
+// example component input
 export const Input: FC<{ name: string }> = () => {
-  return <input className={cx("input")} />
+  return <input className={cx('input')} />
 }
