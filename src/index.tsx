a0f38d6705996cacbbc4cd37994a326c00df258b
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-08-30 15:46:06 -0400

    addred github actions heroku cloud

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,6 @@ import "./index.css"
 import { App } from "./App"
 import { worker } from "./mocks/browser"
 import "./core/i18n/i18n"
-import express from "express"
-
-const app = express()
-
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 3000
-
-app.get("/health", (_, res) => {
-  res.send("ok")
-})
-
-app.use(express.static("build"))
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
 
 if (process.env.NODE_ENV === "development") {
   worker.start()
