f18e1974d428d099337ea531b8229c129b42b729
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-08-31 20:02:05 -0400

    fix code with linter and nodemon restart

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   watchForFileChanges: false,
   includeShadowDom: true,
   e2e: {
-    setupNodeEvents(_) {},
+    setupNodeEvents() {
+      // used for documentation but empty
+    },
     baseUrl: 'http://localhost:3000',
     specPattern: './e2e/specs/**/*.e2e.ts',
     supportFile: false,
